5b762cbe9be4fb44422d59f5218d183926c85e55
archko <archko@gmail.com>
2025-07-20 17:08:28 +0800

    添加返回处理

--- composeApp/src/androidMain/kotlin/com/archko/reader/viewer/App.kt
+++ composeApp/src/androidMain/kotlin/com/archko/reader/viewer/App.kt
@@ -3,25 +3,10 @@ package com.archko.reader.viewer
 import android.net.Uri
 import android.text.TextUtils
 import androidx.activity.compose.BackHandler
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.focusable
+import androidx.compose.foundation.*
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.gestures.scrollBy
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -33,15 +18,7 @@ import androidx.compose.material.icons.automirrored.filled.Toc
 import androidx.compose.material.icons.filled.Error
 import androidx.compose.material.icons.filled.ZoomIn
 import androidx.compose.material.icons.filled.ZoomOut
-import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.VerticalDivider
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
@@ -94,6 +71,15 @@ fun App(
         val errorIcon = rememberVectorPainter(Icons.Default.Error)
         var pdf: LocalPdfState? by remember {
             mutableStateOf(null, referentialEqualityPolicy())
+        }
+
+        BackHandler {
+            val path = viewModel.path
+            if (path.isNullOrEmpty()) {
+                return@BackHandler
+            }
+            pdf = null
+            viewModel.path = null
         }
 
         val scope = rememberCoroutineScope()
