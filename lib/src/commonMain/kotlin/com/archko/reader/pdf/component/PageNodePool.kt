0a8cf0df6264d269b8584736ea5f5b95fa85536f
archko <archko@gmail.com>
2026-01-11 07:11:15 +0800

    node缓存不对,先注释

--- lib/src/commonMain/kotlin/com/archko/reader/pdf/component/PageNodePool.kt
+++ lib/src/commonMain/kotlin/com/archko/reader/pdf/component/PageNodePool.kt
@@ -11,21 +11,21 @@ public class PageNodePool {
 
     // 获取一个 Node，如果没有则新建
     public fun acquire(pageViewState: PageViewState, bounds: Rect, aPage: APage): PageNode {
-        val node = pool.removeLastOrNull()
+        /*val node = pool.removeLastOrNull()
         if (node != null) {
             node.update(bounds, aPage)
             node.recycle()
             return node
-        }
+        }*/
         return PageNode(pageViewState, bounds, aPage)
     }
 
     // 回收 Node
     public fun release(node: PageNode) {
         node.recycle() // 释放位图和 Job
-        if (pool.size < 1000) { // 限制池子大小，防止极端内存占用
+        /*if (pool.size < 1000) { // 限制池子大小，防止极端内存占用
             pool.addLast(node)
-        }
+        }*/
     }
 
     public fun clear() {
